a8dc446b451b8eb320535d9e4ca128b668f9def3
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2022-02-13 22:43:35 +0530

    random changes

---
+++ .config/awesome/startup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Starting some basic utilities
+picom
+pulseaudio
+
+# Starting Bluetooth, and autoconnecting it to my usual bluetooth headset
+sudo systemctl enable bluetooth-autoconnect
+systemctl --user enable pulseaudio-bluetooth-autoconnect
+sudo systemctl enable acpid # required by it
+bluetooth-autoconnect # a downloaded utility to autoconnect trusted and paired devices, but may not work all the time
+
+# # doing it the usual system way, but another utility bluez and bluez-utils are required
+# sudo systemctl enable bluetooth.service
+# rfkill unblock all
+# bluetoothctl power on
+
+# Making directories, and then mounting drives on them
+sudo mkdir /run/media/krishnaraj
+sudo mkdir /run/media/krishnaraj/Extras
+sudo mkdir /run/media/krishnaraj/Windows
+sudo mkdir /run/media/krishnaraj/Photos
+sudo mkdir /run/media/krishnaraj/Miscellaneous
+sudo mkdir /run/media/krishnaraj/Classes
+sudo mount /dev/sda3 /run/media/krishnaraj/Windows
+sudo mount /dev/sda5 /run/media/krishnaraj/Extras
+sudo mount /dev/sda10 /run/media/krishnaraj/Photos
+sudo mount /dev/sda11 /run/media/krishnaraj/Miscellaneous
+sudo mount /dev/sda6 /run/media/krishnaraj/Classes
+sudo chmod ugo+wx /run/media/krishnaraj/Extras
+
+# xinput set-prop "Synaptics TM3336-001" "libinput Tapping Enabled" 1
+
+# # To change the wallpaper
+# feh --bg-fill --randomize ~/Pictures/Wallpapers/Minimalist/*
+
+# # Doing recurring things
+# while sleep 60;
+# do
+# bluetooth-autoconnect;
+# feh --bg-fill --randomize ~/Pictures/Wallpapers/Minimalist/*;
+# bluetoothctl connect A0:60:90:64:D7:2E # connecting to samsung level U
+# done
